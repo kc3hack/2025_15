add8b0a54be8a357d328e62bb62d4289a6cd289a
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-02-19 02:26:28 +0900

    IP作成

--- Idisclosure/Assets/Scenes/Gaming/CreateStoreWeb/CreateStoreWeb.cs
+++ Idisclosure/Assets/Scenes/Gaming/CreateStoreWeb/CreateStoreWeb.cs
@@ -3,6 +3,8 @@ using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
 using TMPro;
+using System.Collections.Generic;
+
 
 public class CreateStoreWeb : MonoBehaviour
 {
@@ -27,8 +29,8 @@ public class CreateStoreWeb : MonoBehaviour
             // 判別しているプロパティをtrueにする
             Hashtable Webs = new Hashtable { { "VirusOO", true } };
             PhotonNetwork.CurrentRoom.SetCustomProperties(Webs);
+            MakeIP("VirusOO");
             Debug.Log("Create VirusOO!!!!!!");
-            // IPの生成処理忘れずに
         }
     }
 
@@ -57,5 +59,44 @@ public class CreateStoreWeb : MonoBehaviour
         {
             ShowBrowser.text = (string)PhotonNetwork.CurrentRoom.CustomProperties["BrowserDisplay"];
         }
+    }
+
+    public void MakeIP(string AppName)
+    {
+        // IPアドレスのリストを取得する
+        List<string> IPList = new List<string>();
+        string NewIP = "";
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("IPList"))
+        {
+            IPList = new List<string>(PhotonNetwork.CurrentRoom.CustomProperties["IPList"].ToString().Split(','));
+        }
+
+        // 一意性の確認と追加
+        while (true)
+        {
+            // IPを生成
+            int first = UnityEngine.Random.Range(0, 256);
+            int second = UnityEngine.Random.Range(0, 256);
+            int third = UnityEngine.Random.Range(0, 256);
+            int fourth = UnityEngine.Random.Range(0, 256);
+
+            NewIP = $"{first}.{second}.{third}.{fourth}";
+            
+            // 一意性が保証されたら追加
+            if (!(IPList.Contains(NewIP)))
+            {
+                IPList.Add(NewIP);
+                break;
+            }
+        }
+
+        // サーバー上に保存
+        Hashtable props = new Hashtable
+        {
+            { "IPList", string.Join(",", IPList) },
+            { AppName + "IP", NewIP }
+        };
+        PhotonNetwork.CurrentRoom.SetCustomProperties(props);
+        Debug.Log("IPList is saved! : " + string.Join(",", IPList));
     }
 }
