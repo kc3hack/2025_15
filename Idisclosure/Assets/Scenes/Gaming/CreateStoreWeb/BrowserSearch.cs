fcf585093ffea9ef7d655580e4e13d4a81dd22e6
Kyarasu0 <Setsuru.lilac@gmail.com>
2025-02-19 13:21:05 +0900

    IPをWiFiに保存

--- Idisclosure/Assets/Scenes/Gaming/CreateStoreWeb/BrowserSearch.cs
+++ Idisclosure/Assets/Scenes/Gaming/CreateStoreWeb/BrowserSearch.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 using TMPro;
 using ExitGames.Client.Photon;
 using Photon.Pun;
+using System.Linq;
 
 public class BrowserSearch : MonoBehaviour
 {
@@ -21,12 +22,28 @@ public class BrowserSearch : MonoBehaviour
             int Battery = int.Parse(PlayerPrefs.GetString("Battery", "0").Replace("\u200B", ""));
             if ((Battery - drain >= 0))
             {
+                /*----------Battery減算処理----------*/
                 Battery -= drain;
                 PlayerPrefs.SetString("Battery", Battery.ToString());
                 PlayerPrefs.Save();
+                /*----------WiFiにIPを記録----------*/
+                // Wifi番号を取得
+                int WifiNumber = int.Parse(PlayerPrefs.GetString("WifiNumber", "1"));
+                // SNSServerIPを取得
+                if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("SNSServerIP"))
+                {
+                    string SNSServerIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["SNSServerIP"];
+                }
+                // 自分のIPを取得
+                if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("PlayerIP"))
+                {
+                    string PlayerIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["PlayerIP"];
+                }
+                SortAndSave(SNSServerIP, PlayerIP, WifiNumber);
+                /*----------シーン遷移----------*/
                 SceneManager.LoadScene("SNSServer");
             }
-            // ここにwifiにIPメモる処理忘れずに
+
         }
         
         /*----------Virus Osakano Obatyannを検索----------*/
@@ -41,12 +58,27 @@ public class BrowserSearch : MonoBehaviour
                     int Battery = int.Parse(PlayerPrefs.GetString("Battery", "0").Replace("\u200B", ""));
                     if ((Battery - drain >= 0))
                     {
+                        /*----------バッテリー減算処理----------*/
                         Battery -= drain;
                         PlayerPrefs.SetString("Battery", Battery.ToString());
                         PlayerPrefs.Save();
+                        /*----------WiFiにIPを記録----------*/
+                        // Wifi番号を取得
+                        int WifiNumber = int.Parse(PlayerPrefs.GetString("WifiNumber", "1"));
+                        // SNSServerIPを取得
+                        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("FishingVirusOOIP"))
+                        {
+                            string FishingVirusOOIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["FishingVirusOOIP"];
+                        }
+                        // 自分のIPを取得
+                        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("PlayerIP"))
+                        {
+                            string PlayerIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["PlayerIP"];
+                        }
+                        SortAndSave(FishingVirusOOIP, PlayerIP, WifiNumber);
+                        /*----------シーン遷移----------*/
                         SceneManager.LoadScene("FishingVirusOO");
                     }
-                    // ここにwifiにIPをメモる処理忘れずに
                 }
             }
             else
@@ -58,14 +90,65 @@ public class BrowserSearch : MonoBehaviour
                     int Battery = int.Parse(PlayerPrefs.GetString("Battery", "0").Replace("\u200B", ""));
                     if ((Battery - drain >= 0))
                     {
+                        /*----------バッテリー減算処理----------*/
                         Battery -= drain;
                         PlayerPrefs.SetString("Battery", Battery.ToString());
                         PlayerPrefs.Save();
+                        /*----------WiFiにIPを記録----------*/
+                        // Wifi番号を取得
+                        int WifiNumber = int.Parse(PlayerPrefs.GetString("WifiNumber", "1"));
+                        // SNSServerIPを取得
+                        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("VirusOOIP"))
+                        {
+                            string VirusOOIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["VirusOOIP"];
+                        }
+                        // 自分のIPを取得
+                        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("PlayerIP"))
+                        {
+                            string PlayerIP = (string)PhotonNetwork.CurrentRoom.CustomProperties["PlayerIP"];
+                        }
+                        SortAndSave(VirusOOIP, PlayerIP, WifiNumber);
+                        /*----------シーン遷移----------*/
                         SceneManager.LoadScene("VirusOO");
                     }
-                    // ここにwifiにIPをメモる処理忘れずに
                 }
             }
         }
     }
+
+    public void SortAndSave(string IP1, string IP2, int WifiNumber)
+    {   
+        int ScaleIP1 = int.Parse(IP1.Replace(".", ""));
+        int ScaleIP2 = int.Parse(IP2.Replace(".", ""));
+        int[] IPScales = { ScaleIP1, ScaleIP2 };
+        int[] IPScalesClone = (int[])IPScales.Clone();
+        Array.Sort(IPScales);
+
+        // ソート後の配列と元の配列を比較
+        string NewRecord;
+        if (IPScalesClone.SequenceEqual(IPScales))
+        {
+            // IPが昇順で並んでいる場合
+            NewRecord = IP1 + ":" + IP2 + "\n";
+        }
+        else
+        {
+            // IPが降順の場合
+            NewRecord = IP2 + ":" + IP1 + "\n";
+        }
+
+        // WiFiのカスタムプロパティを更新
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("WiFi" + WifiNumber))
+        {
+            string Record = (string)PhotonNetwork.CurrentRoom.CustomProperties["WiFi" + WifiNumber];
+            NewRecord = Record + NewRecord;
+        }
+
+        Hashtable props = new Hashtable
+        {
+            { "WiFi" + WifiNumber, NewRecord },
+        };
+        PhotonNetwork.CurrentRoom.SetCustomProperties(props);
+    }
+
 }
--- Idisclosure/Assets/Scenes/Waiting/ReceiveIPs.cs
+++ Idisclosure/Assets/Scenes/Waiting/ReceiveIPs.cs
@@ -15,9 +15,18 @@ public class ReceiveIPs : MonoBehaviourPunCallbacks, IOnEventCallback
             object[] receivedData = (object[])photonEvent.CustomData;
             string playerIP = (string)receivedData[0];
             string serverIP = (string)receivedData[1];
+            // ローカル保存
             PlayerPrefs.SetString("PlayerIP", playerIP);
             PlayerPrefs.SetString("ServerIP", serverIP);
             PlayerPrefs.Save();
+            // サーバー保存
+            Hashtable props = new Hashtable
+            {
+                { "PlayerIP", PlayerIP },
+                { "ServerIP", ServerIP }
+            };
+            PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+
             Debug.Log("IPs are saved! : " + playerIP + " : " + serverIP);
         }
     }
